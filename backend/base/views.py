258c52bd155e70aceb5cac6bede3ced11508a5b1
chydeepak7 <deepakchy793@gmail.com>
2024-06-05 19:19:50 +0545

    started learning login with email

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
-
+from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 # Create your views here.
 
 
@@ -16,11 +19,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
+
     def validate(self,attrs):
         data = super().validate(attrs)
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
         return data
 
     
@@ -31,11 +35,20 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
-    files = Files.objects.all()
-    serializer = FileSerializer(files, many=True)
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)
+
 
 
 @api_view(["GET"])
@@ -43,3 +56,20 @@ def get_files(request):
     files = Files.objects.all()
     serializer = FileSerializer(files, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    try:
+
+        data = request.data
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
